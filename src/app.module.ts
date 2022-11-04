ff8586148b7e6a766549dda15410e3e915ee4a91
Ariel31 <arielf_steps@bitbucket.org>
2022-11-04 23:40:07 +0200

    added base modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { PaymentsModule } from './payments/payments.module';
+import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PaymentsModule, RestaurantModule],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
