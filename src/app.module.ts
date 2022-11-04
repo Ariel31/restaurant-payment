77401fa49ad453a8f940313f2c961c5a0eeb1e1a
Ariel31 <arielf_steps@bitbucket.org>
2022-11-04 23:57:07 +0200

    db setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,23 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { ConfigService } from '@nestjs/config/dist';
+import { MongooseModule } from '@nestjs/mongoose';
 import { PaymentsModule } from './payments/payments.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
 
 @Module({
-  imports: [PaymentsModule, RestaurantModule],
+  imports: [
+    ConfigModule.forRoot(),   
+    PaymentsModule,
+    RestaurantModule,
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get<string>('MONGODB_URI'),
+      }),
+      inject: [ConfigService],
+    }),
+  ],
   controllers: [],
   providers: [],
 })
